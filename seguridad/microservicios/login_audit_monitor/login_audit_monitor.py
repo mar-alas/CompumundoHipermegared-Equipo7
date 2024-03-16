593936b86d9290b7cd00f12f97a27164bf661068
Daniel Gamez <dgamez@porvenir.com.co>
2024-03-15 23:29:57 -0500

    ajuste en componente login_audit_monitor

--- seguridad/microservicios/login_audit_monitor/login_audit_monitor.py
+++ seguridad/microservicios/login_audit_monitor/login_audit_monitor.py
@@ -1,17 +1,15 @@
+#autor: Daniel Gamez
 #este codigo revisa si en los lgos login_logs.csv hay usuarios con mas de 3 logins no exitosos
 # e imprime el resultado en el archivo logs_login_audit_monitor.txt
+#este codigo se debe correr desde la base del proyecto
+
 import pandas as pd
 import time
 import os
 
 umbral_login_no_exitoso = 3
 
-
-ruta_logs_auditor="seguridad/microservicios/login_audit_monitor/logs_login_audit_monitor.txt"
 ruta_logs_auditor_csv="seguridad/microservicios/base_datos/table_logs_login_audit_monitor.csv"
-
-if os.path.exists(ruta_logs_auditor):
-            os.remove(ruta_logs_auditor)
 
 #if os.path.exists(ruta_logs_auditor_csv):
 #            os.remove(ruta_logs_auditor_csv)
@@ -30,9 +28,7 @@ def revision_logins_no_exitosos():
     if not logins_no_exitosos_por_usuario_sospechosos.empty:
         mensaje= " ALERTA Usuarios con mas de "+ str(umbral_login_no_exitoso) +" logins no exitosos"
         print(fecha + mensaje)
-        with open(ruta_logs_auditor, 'a') as f:
-            f.write(mensaje + "\n")
-
+       
         existe_csv=os.path.exists(ruta_logs_auditor_csv)
         df_log = pd.DataFrame({'Fecha': [fecha],'Tipo_Alerta':'MULTIPLE_LOGINS', 'Mensaje': [mensaje]})
         df_log.to_csv(ruta_logs_auditor_csv, mode='a', header=not(existe_csv), index=False, sep=';')
@@ -40,16 +36,15 @@ def revision_logins_no_exitosos():
     else:
         mensaje=fecha + "No hay usuarios con mas de 3 logins no exitosos"
         print(mensaje)
-        with open(ruta_logs_auditor, 'a') as f:
-            f.write(mensaje+"\n")
+        
 
-contador = 0
-while contador<3:
-    revision_logins_no_exitosos()
-    contador += 1
-    time.sleep(2)
+try:
+    while True:
+        revision_logins_no_exitosos()
+        time.sleep(2)
 
-
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt received. Exiting...")
 
 
 
