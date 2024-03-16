d0b0fa12369ce9141e9f6588a2433cce9b24c723
Robert Castro <ra.castro2@uniandes.edu.co>
2024-03-16 07:38:08 -0500

    Ajuste coneccion_bd.py

--- seguridad/microservicios/base_datos/coneccion_bd.py
+++ seguridad/microservicios/base_datos/coneccion_bd.py
@@ -6,15 +6,17 @@ def validar_datos_usuario(username, password):
     # Open the CSV file
     with open("seguridad/microservicios/base_datos/table_usuarios.csv", mode='r') as file:
         # Create a CSV reader object
-        reader = csv.reader(file, delimiter=';')
+        # reader = csv.reader(file, delimiter=';')
+        reader = csv.DictReader(file, delimiter=';')
         # Skip the header if present
         next(reader, None)
         # Loop through each row in the CSV file
         for row in reader:
             # Extract email and password from the row
-            email, password, _ = row
+            # email, password, _ = row
             # Add the email and password to the dictionary
-            users[email.strip()] = password.strip()
+            # users[email.strip()] = password.strip()
+            users[row['usuario'].strip()] = row['contrasenia'].strip()
     if username in users and users[username] == password:
         return True
     else:
